5c59ec757ff22cddcb7c26dffee5fe12e96caf08
ktomoeda <30852859+ktomoeda@users.noreply.github.com>
2018-03-24 18:17:14 -0500

    added manual entry mode

--- app/src/main/java/com/akinc/myhanger/ManualActivity.java
+++ app/src/main/java/com/akinc/myhanger/ManualActivity.java
@@ -2,6 +2,8 @@ package com.akinc.myhanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ManualActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class ManualActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
     }
+    //get the spinner from the xml.
+    Spinner dropdown = findViewById(R.id.spinner);
+    //create a list of items for the spinner.
+    String[] items = new String[]{"United", "American", "Delta"};
+    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 }
